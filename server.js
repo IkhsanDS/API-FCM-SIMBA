eb130d14e5ee360ca4d132b33327b63a9add1908
IKHSAN DWI SETO <ikhsanseto@gmail.com>
2025-07-17 06:21:50 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serverkey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://monitoringbanjiramikom-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const db = admin.database();
+const statusRef = db.ref('data/status_air');
+
+let lastStatus = "";
+
+statusRef.on('value', async (snapshot) => {
+  const status = snapshot.val();
+
+  if (status !== lastStatus) {
+    console.log(`⚠️ Status air berubah: ${status}`);
+    lastStatus = status;
+
+    const message = {
+      token: 'cS12UGIJScqbW9hsCQWXC_:APA91bHIY1iOUSTPhHzYE4xVB29m4ri8X1E0zX9eXZxAc5jJNPwsMeQaG3WiYJkuFFhOxKzyegqd83Nor9ICkuyqVhvOiGgmfxKXhK2vgTrjEOu5ohsxUKk',
+      notification: {
+        title: '⚠️ Peringatan Banjir',
+        body: `Status air sekarang: ${status.toUpperCase()}`
+      }
+    };
+
+    try {
+      const response = await admin.messaging().send(message);
+      console.log('✅ Notifikasi dikirim:', response);
+    } catch (error) {
+      console.error('❌ Gagal mengirim notifikasi:', error);
+    }
+  } else {
+    console.log('ℹ️ Tidak ada perubahan status air');
+  }
+});
